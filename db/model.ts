420668f9b25f897a59dd9d608846aca744934d69
toan5ks1 <vipteamvnclass@gmail.com>
2025-02-19 21:34:52 +0700

    feat: add mockup data for backend

---
+++ db/model.ts
@@ -0,0 +1,30 @@
+export interface IProduct {
+  id: number;
+  title: string;
+  category:
+    | "Upper Body"
+    | "Lower Body"
+    | "Hat"
+    | "Shoes"
+    | "Accessory"
+    | "Legendary"
+    | "Mythic"
+    | "Epic"
+    | "Rare";
+  price: number;
+  isFavorite: boolean;
+  createdAt: number;
+  theme: "Dark" | "Light" | "Colorful" | "Halloween";
+  tier: "Basic" | "Premium" | "Deluxe";
+  imageId: number; // 1 -> 20 (integer)
+  author: IAuthor;
+}
+
+export interface IAuthor {
+  firstName: string;
+  lastName: string;
+  email: string;
+  gender: string;
+  avatar: string;
+  onlineStatus: string;
+}
